14649dfbb438996e347091afb0c5004afedee2e5
石纯勇 <shichunyong@scy-macbook-pro.local>
2016-09-09 10:22:44 +0800

    Initial Commit

---
+++ MyPSWTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPSWTextField
+//
+//  Created by 石纯勇 on 16/9/9.
+//  Copyright © 2016年 石纯勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyPSWTextField/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyPSWTextField
+//
+//  Created by 石纯勇 on 16/9/9.
+//  Copyright © 2016年 石纯勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
